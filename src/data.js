f3ded1c7d64a471d79134535f1f55737e7479de1
laxman989 <laxmanchoudhary5685@gmail.com>
2023-12-24 10:24:08 +0530

    Travel DilSe added to gitHub

---
+++ src/data.js
@@ -0,0 +1,145 @@
+const packages = [
+    {
+        id: 1,
+        image: "https://images.pexels.com/photos/2748019/pexels-photo-2748019.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title: "Italy",
+        subTitle: "Italy is great place to visit.",
+        price: 400,
+        rating: 3.9
+    },
+    {
+        id: 2,
+        image: "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "France",
+        subTitle: "France is great place to visit.",
+        price: 260,
+        rating: 3.2
+    },
+    {
+        id: 3,
+        image: "https://images.pexels.com/photos/572689/pexels-photo-572689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "New Zealand",
+        subTitle: "New Zealand is great place to visit.",
+        price: 190,
+        rating: 4.9
+    },
+    {
+        id: 4,
+        image: "https://images.pexels.com/photos/1588032/pexels-photo-1588032.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "India",
+        subTitle: "India is great place to visit.",
+        price: 340,
+        rating: 4.3
+    },
+    {
+        id: 5,
+        image: "https://images.pexels.com/photos/2615031/pexels-photo-2615031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "Australia",
+        subTitle: "Australia is great place to visit.",
+        price: 360,
+        rating: 4.7
+    },
+    {
+        id: 6,
+        image: "https://images.pexels.com/photos/122244/pexels-photo-122244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "Malaysia",
+        subTitle: "Malaysia is great place to visit.",
+        price: 410,
+        rating: 3.8
+    },
+    {
+        id: 7,
+        image: "https://images.pexels.com/photos/2166553/pexels-photo-2166553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "Indonesia",
+        subTitle: "Indonesia is great place to visit.",
+        price: 310,
+        rating: 3.9
+    },
+    {
+        id: 8,
+        image: "https://images.pexels.com/photos/2915957/pexels-photo-2915957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "China",
+        subTitle: "China is great place to visit.",
+        price: 270,
+        rating: 3.4
+    },
+    {
+        id: 9,
+        image: "https://images.pexels.com/photos/1774389/pexels-photo-1774389.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "United States",
+        subTitle: "United States is great place to visit.",
+        price: 460,
+        rating: 4.0
+    },
+]
+
+const services = [
+    {
+        id: 1,
+        name: "Food & Drinks",
+        description: "Engage travelers in interactive and educational experiences related to food and beverages"
+    },
+    {
+        id: 2,
+        name: "Transportation Assistance",
+        description: "Arrangement of airport transfers, car rentals, and transportation between destinations"
+    },
+    {
+        id: 3,
+        name: "Wellness & Spa",
+        description: "Wellness retreats and spa for relaxation and rejuvenation"
+    },
+    {
+        id: 4,
+        name: "Safety Guide",
+        description: "Be aware of your surroundings and trust your instincts"
+    },
+    {
+        id: 5,
+        name: "Affordable Hotel",
+        description: "Booking and reservation services for hotels, resorts, lodges, and vacation rentals"
+    },
+    {
+        id: 6,
+        name: "Travel Insurance",
+        description: "Offer travel insurance options to ensure travelers' safety and security during their trips"
+    },
+]
+
+const galleryImages = [
+    {
+        id: 1,
+        imageUrl: "https://images.pexels.com/photos/1004584/pexels-photo-1004584.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 2,
+        imageUrl: "https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 3,
+        imageUrl: "https://images.pexels.com/photos/5254688/pexels-photo-5254688.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 4,
+        imageUrl: "https://images.pexels.com/photos/358298/pexels-photo-358298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 5,
+        imageUrl: "https://images.pexels.com/photos/815880/pexels-photo-815880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 6,
+        imageUrl: "https://images.pexels.com/photos/1334441/pexels-photo-1334441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 7,
+        imageUrl: "https://images.pexels.com/photos/2034335/pexels-photo-2034335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 8,
+        imageUrl: "https://images.pexels.com/photos/5918475/pexels-photo-5918475.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    
+]
+
+export { packages, services, galleryImages};
